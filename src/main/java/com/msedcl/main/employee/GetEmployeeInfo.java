b8915263b1732083023a898d1e5dd20ecfdd0f8b
Admin <Admin@DESKTOP-IA2TH3O>
2026-05-25 14:12:52 +0530

    new class GetEmployeeInfo  added

---
+++ src/main/java/com/msedcl/main/employee/GetEmployeeInfo.java
@@ -0,0 +1,9 @@
+package com.msedcl.main.employee;
+
+public class GetEmployeeInfo {
+	public static void showInfo() {
+		System.out.println(" Show Employee INfo from update branch");
+		System.out.println(" Show Employee INfo name from delete branch");
+	}
+
+}
